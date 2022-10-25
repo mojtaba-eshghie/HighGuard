be1f072e7ea682529fcf2c1c09a526e6934897d8
Mojtaba Eshghie <eshghie@kth.se>
2022-10-25 12:58:41 +0200

    adding the contract source code

---
+++ contracts/sample.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.13 and less than 0.9.0
+pragma solidity ^0.8.17;
+
+contract HelloWorld {
+    bool public oneIsDone = false;
+
+    event OneInvoked();
+    event TwoInvoked();
+
+    modifier before() {
+        require(oneIsDone);
+        
+        _;
+    }
+
+    function one() public {
+        oneIsDone = true;
+        emit OneInvoked();
+    }
+
+    
+    function two() before public {
+        emit TwoInvoked(); 
+    }
+}
