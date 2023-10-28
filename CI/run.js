7b2eef5734b22895160bf91ae8b42987e4ff859c
Mojtaba Eshghie <eshghie@kth.se>
2023-10-28 14:10:06 +0200

    Changed logger to  store the results in the log files based on date;

--- CI/run.js
+++ CI/run.js
@@ -8,6 +8,8 @@ let { terminateProcessByPid } = require('@lib/os/process');
 let yargs = require('yargs/yargs');
 let { hideBin } = require('yargs/helpers');
 let logger = require('@lib/logging/logger');
+let { readCIConfig } = require('@lib/config');
+
 
 
 let argv = yargs(hideBin(process.argv))
@@ -25,16 +27,6 @@ if (argv.verbose) {
 }
 
 
-/**
- * Reads the CI configuration from ci-config.yml.
- * 
- * @returns {Object} The CI configuration.
- */
-function readCIConfig() {
-    let ciConfigPath = path.join(__dirname, 'ci-config.yml');
-    let ciConfigContent = fs.readFileSync(ciConfigPath, 'utf8');
-    return yaml.load(ciConfigContent);
-}
 
 /**
  * Sets up the environments and runs the tests based on the CI configuration.
@@ -49,7 +41,7 @@ async function setupAndRunTests() {
         let environment = test.environment;
         let testFiles = test.files;
 
-        logger.info(chalk.blue(`> Setting up environment: [${environment}]`));
+        logger.info(chalk.blue(`Setting up environment: [${environment}]`));
         let envInfo = null;
         let web3 = null; 
 
@@ -59,10 +51,13 @@ async function setupAndRunTests() {
             web3 = env['web3']
         }
 
-        logger.info(chalk.green(`> Running exploits for environment: [${environment}]`));
+        logger.info(chalk.green(`Running exploits for environment: [${environment}] \n`));
         for (let testFile of testFiles) {
             let testFilePath = path.join(__dirname, test.directory, testFile);
-            logger.info(chalk.cyan(`${'- '.repeat(50)+'\n\n'}Executing tests from: [${testFile}]`));
+
+            logger.info(chalk.cyan(`${'- '.repeat(40)+'\n'}`));
+            logger.info(chalk.cyan(`Executing tests from: [${testFile}]`));
+
             let testModule = require(testFilePath);
             if (typeof testModule === 'function') {
                 let result = await testModule(web3, envInfo);
@@ -71,24 +66,26 @@ async function setupAndRunTests() {
                 } else {
                     failedExploits++;
                 }
+            } else {
+                logger.debug(chalk.blue(`Failed to fetch the correct function to run.`))
             }
         }
 
-        logger.debug(chalk.blue("+ All reporting operations should finish before freeing environment resources."))
+        logger.debug(chalk.blue("All reporting operations should finish before freeing environment resources."))
 
 
         // Close environment. Without closing the environment properly, resources will be wasted.
         web3.currentProvider.disconnect();
         terminateProcessByPid(envInfo.pid);
-        logger.debug(chalk.blue("+ Terminated any running anvil instance."))
+        logger.debug(chalk.blue(`Terminated running anvil instance with PID: ${envInfo.pid}`));
     }
 
     // Display the results  
-    logger.info(chalk.cyan('\n'+'= '.repeat(50)+'\n'));
+    logger.info(chalk.cyan('= '.repeat(40)+'\n'));
     logger.info(chalk.cyan('Finished executing all exploits.\n'));
     logger.info(chalk.green(`Total successful exploits: ${successfulExploits}`));
     logger.info(chalk.red(`Total failed exploits: ${failedExploits}\n`));
-    logger.info(chalk.cyan('\n'+'= '.repeat(50)));
+    logger.info(chalk.cyan('= '.repeat(40)));
     process.exit(0);
 }
 
