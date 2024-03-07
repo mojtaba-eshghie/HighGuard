e5a8df59a234ee072c0fa6baa0e97e1a20e71fe0
Mojtaba Eshghie <eshghie@kth.se>
2024-03-07 14:27:18 +0100

    Feature: updating plugin-based convention system #58

---
+++ CI/conventions/plugins/index.js
@@ -0,0 +1,6 @@
+/**
+ * This index.js file will be called by other parts of the monitor (such as run.js or tests).
+ * The practice is to put plugin conventions as separate js files under conventions/plugins. 
+ * Import them here in this index file, then, execute the ones that are passed to plugins index 
+ * file as parameters.
+ */
