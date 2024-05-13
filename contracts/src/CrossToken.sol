ad71f87c5105cf6548005c065b44dafb7d8f3d63
Hans Stammler <81676569+hansstammler@users.noreply.github.com>
2024-05-13 16:09:08 +0200

    Feat #89: Added bridging contracts
    Added source, destination and token contracts for wrapping native tokens on source chain and sending over bridge

---
+++ contracts/src/CrossToken.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CrossToken is ERC20 {
+    
+    address owner; 
+    constructor() ERC20("CrossToken", "CT") {
+        //_mint(msg.sender, 1000*10**18);
+        owner = msg.sender;
+
+    }
+    //Relinquish ownership to 
+    function changeOwner(address newOwner) public {
+        if(msg.sender == owner) {
+            owner = newOwner;
+        }
+    }
+
+    function issueToken(address destination, uint amount) public {
+        if(msg.sender == owner) {
+            _mint(destination, amount);
+        }
+    }
+
+    function burnToken(uint amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function testToken(address destination, uint amount) public {
+        _mint(destination, amount);
+    }
+}
